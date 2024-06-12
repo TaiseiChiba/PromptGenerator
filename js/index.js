9f5bb331e85a694254607b5e50d4f4571409436e
Taisei Chiba <nanakorobiyaoki.tai@icloud.com>
2024-06-12 18:56:56 +0900

    first commit

---
+++ js/index.js
@@ -0,0 +1,89 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+
+  // 「作成」ボタン押下時のイベント
+  generateEvent();
+
+  // 「プロンプトをコピーしてChatGPTへ画面遷移」ボタン押下時のイベント
+  copyEvent();
+});
+
+/**
+ * 「作成」ボタン押下時のイベント
+ */
+const generateEvent = function() {
+  const btnGenerate = document.querySelector('#btn-generate');
+  btnGenerate.addEventListener('click', function() {
+    generatePrompt();
+  });
+}
+
+/**
+ * 「プロンプトをコピーしてChatGPTへ画面遷移」ボタン押下時のイベント
+ */
+const copyEvent = function() {
+  const btnCopy = document.querySelector('#btn-copy');
+  btnCopy.addEventListener('click', function() {
+    copyPromptAndMoveChatGPT();
+  });
+}
+
+/**
+ * プロンプトを作成します。
+ */
+const generatePrompt = function() {
+  // ラジオボタンの値を取得
+  const inheritanceValue = document.querySelector('input[name="inheritance"]:checked').value;
+  const interfaceValue = document.querySelector('input[name="interface"]:checked').value;
+  const arrayListValue = document.querySelector('input[name="arrayList"]:checked').value;
+  const arrayValue = document.querySelector('input[name="array"]:checked').value;
+  const delegateValue = document.querySelector('input[name="delegate"]:checked').value;
+
+  // プロンプト作成
+  const prompt = [
+    'Java',
+    '＜要件＞',
+    '・mainメソッドはStartUp.javaに書きます。',
+    '・mianメソッドで行う処理の手順を箇条書きで丁寧に書いてください。',
+    '・mianメソッド実行後、コンソールに出力される画面表示例をつけてください。',
+    '・クラス名とクラスを構成する属性と操作の一覧を出力してください。',
+    '',
+    '＜条件＞',
+    `・継承: ${inheritanceValue}`,
+    `・インターフェイス: ${interfaceValue}`,
+    `・ArrayList: ${arrayListValue}`,
+    `・配列: ${arrayValue}`,
+    `・操作の委譲: ${delegateValue}`,
+    '',
+    '＜ルール＞',
+    '・標準入力は使わない。',
+    '',
+    '上記の要件と条件、ルールを満たすユニークな問題を作成してください。',
+    '問題の作成にはポリモーフィズムを使用してください。',
+    '出力内容は、「問題の概要」「mainメソッド処理手順」「画面表示例」「クラス名とクラスを構成する属性と操作」の順で出力してください。',
+    '答えとなるコードはまだ出力しないでください。'].join('\n');
+
+  // pre要素にプロンプトを挿入
+  const preElement = document.querySelector('#promptArea pre');
+  preElement.textContent = prompt;
+
+  // d-noneを解除
+  const promptArea = document.querySelector('#promptArea');
+  promptArea.classList.remove("d-none");
+}
+
+/**
+ * プロンプトをコピーしてChatGPTへ画面遷移します。
+ */
+const copyPromptAndMoveChatGPT = function() {
+  const preElement = document.querySelector('#promptArea pre');
+  const prompt = preElement.textContent;
+  // promptをコピー
+  navigator.clipboard.writeText(prompt).then(
+    () => {
+      // 新しいタブを開き、ページを表示
+      window.open('https://chat.openai.com/', '_blank'); 
+    },
+    () => {
+      alert('promptのコピーに失敗しました');
+    });
+}
